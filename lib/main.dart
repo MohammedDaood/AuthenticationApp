e0d7247e00ee896dbb786906e0b5c1484bce4c35
Mohammed Daood <a3em445439@gmail.com>
2026-06-04 04:11:29 +0300

    update the font

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: GoogleFonts.cairo().fontFamily),
+      theme: ThemeData(fontFamily: 'Cairo'),
       debugShowCheckedModeBanner: false,
       home: isCompleted ? const HomeScreen() : const OnboardingScreen(),
     );
